84d9c9e48b56e577e6e1c2820d4ba844d4f432cd
JordanVo <jordan.vo@ou.edu>
2018-11-29 04:52:03 -0600

    Add files via upload
    Child theme I worked on via a Lynda tutorial.

---
+++ popper-child-but-better/footer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package popper
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php
+		if (is_active_sidebar( 'sidebar-2' ) ) { ?>
+			<div id="tertiary" class="widget-area clear" role="complementary">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div><!-- #secondary -->
+		<?php }
+		?>
+
+    <?php if ( has_nav_menu( 'secondary' ) ) : ?>
+			<nav id="footer-navigation" class="footer-navigation clear" role="navigation">
+				<div class="the-footer-menu centered">
+					<?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'secondary',
+              'menu_id' => 'secondary-menu',
+              'depth' => 1
+            )
+          ); ?>
+				</div>
+
+			</nav><!-- #site-navigation -->
+		<?php endif; ?>
+
+    <div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'popper-child' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'popper-child' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'popper-child' ), 'popper-child-but-better', '<a href="http://mor10.com" rel="designer nofollow">Jordan Vo</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
